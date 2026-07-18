617fc9009432bb279d58cf77beda98e6969ba8fe
Kiplangat22 <kiplangatkevin00@gmail.com>
2026-07-18 18:52:19 +0300

    refactor(server): modularize health endpoint

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
+import express from "express";
 import healthRoutes from "../modules/health/health.routes.js";
 
-const router = Router();
+const router: express.Router = express.Router();
 
 router.use("/health", healthRoutes);
 
